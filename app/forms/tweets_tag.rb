6b2ce838ad464a7364d099564e53a0c5a2322723
progate-bigin <69100896+progate-bigin@users.noreply.github.com>
2020-09-16 16:37:05 +0900

    クラス名の修正

--- app/forms/tweets_tag.rb
+++ app/forms/tweets_tag.rb
@@ -1,4 +1,4 @@
-class TweetTag
+class TweetsTag
   include ActiveModel::Model
   attr_accessor :message, :name
 
@@ -9,7 +9,8 @@ class TweetTag
 
   def save
     tweet = Tweet.create(message: message)
-    tag = Tag.create(name: name)
+    tag = Tag.where(name: name).first_or_initialize
+    tag.save
 
     TweetTagRelation.create(tweet_id: tweet.id, tag_id: tag.id)
   end
